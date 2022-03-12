741f4e12fa5936c2cd0b56eb6a012e4231a4fa1b
Dario Lostumbo <dario.lostumbo@hotmail.com>
2022-03-13 00:14:47 +0100

    test_lassst

--- Route/index.js
+++ Route/index.js
@@ -2,15 +2,18 @@
 
 module.exports=(app)=>{
 
-    app.get("/api",(req,res)=>{
+    app.get("/api/message",(req,res)=>{
         console.log('INSIDE REQ')
 
         //const userSend=JSON.stringify(userFake)
-        res.send("Hollllaaaa")
+        //res.send("Hollllaaaa")
         //res.json(userFake)
+        res.status(200).send({message:"Hola"})
     })
 
     app.get('*', (req,res)=>{
         res.sendFile(path.join(__dirname + '/client/build/index.html'))
     })
+
+
 }
