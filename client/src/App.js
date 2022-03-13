e09f6812ce9417459100a1eda1aa1b344746334d
Dario Lostumbo <dario.lostumbo@hotmail.com>
2022-03-13 23:46:10 +0100

    13032022_prod

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {messageResult.message} Test
+          {messageResult.message} Test Production
         </p>
         <a
           className="App-link"
